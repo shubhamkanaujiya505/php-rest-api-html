f52d547772ab1de0a9e915f022576eb59c126c8a
shubham kanaujiya <shubhamkanaujiya505@gmail.com>
2025-05-15 14:38:22 +0530

    add insert query

--- api-insert.php
+++ api-insert.php
@@ -1,5 +1,18 @@
 <?php
+header('Content-Type: application/json');
+header('Access-Control-allow-Origin: *');
+include "config.php";
 
+$data = json_decode(file_get_contents("php://input"), true);
+// print_r($data);die;
+$name = $data ["name"];
+$email = $data["email"];
+$phone = $data["phone"];
+$sql = "insert into api_data(name,email,phone) values('{$name}','{$email}','{$phone}')";
 
-
+if (mysqli_query($conn, $sql)) {
+    echo json_encode(array('message' => 'Record Inserted Successfully', 'status' => true));
+} else {
+    echo json_encode(array('message' => 'No Record Found', 'status' => false));
+}
 ?>
